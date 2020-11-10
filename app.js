64c13bf7d8b52cf2c51fbc89e65d65a9a1895fa9
ludwigcarlsson <zhendiac@gmail.com>
2020-11-10 09:48:33 +0100

    feat: working input and output, TODO: display OS

--- app.js
+++ app.js
@@ -2,13 +2,53 @@
 // 2. Display OS info
 // 3. Start HTTP server
 // input: 1-3 else 'Invalid option'
+const fs = require('fs');
+const os = require('os');
+const http = require('http');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 console.log("Choose an option:\n1. Read package.json\n2. Display OS info\n3. Start HTTP server");
-console.log("Type a number:");
+readline.question(`Type a number: `, (choice) => {
+    
+    if (choice < 1 || choice > 3) {
+        console.log("Invalid number");
+    } else {
+        switch (choice) {
+            case "1":
+                fs.readFile(__dirname + '/package.json', 'utf-8', (err, content) => {
+                    console.log(content);
+                });
+                break;
+            case "2": 
+                console.log('display');
+                break;
+            case "3":
+                runServ()
+                break;
+            default:
+                break;
+        } 
+    }
 
-
-const fs = require('fs');
-
-fs.readFile(__dirname + '/package.json', 'utf-8', (err, content) => {
-    //console.log(content);
+    readline.close();
 })
+
+
+function runServ() {
+    const server = http.createServer((req, res) => {
+        if (req.url === "/") {
+            res.write('Hello World!');
+            res.end();
+        }
+
+        if (req.url === "/project") {
+            res.write(JSON.stringify( ['JS', 'Node'] ));
+            res.end();
+        }
+    });
+
+    server.listen(3000);
+}
